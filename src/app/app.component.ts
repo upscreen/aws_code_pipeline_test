3953e178eef8e20b4a11b67d07d2805c5388a1e7
dev03 <omar@toracom.net>
2017-08-17 10:33:59 -0500

    Comit 2
    Cambio para probar aws code pipe line

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
 
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  public title = 'app';
+  public time:Date = new Date();
+
+  public constructor() {
+    setInterval(() => {
+      this.time = new Date();
+    },1000);
+  }
 }
